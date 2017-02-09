7d7711177fbc08abc924a986c5f1c2715d99421b
Lilian Burdianov <lilian.burdianov@gmail.com>
2017-02-09 22:08:01 +0400

    Add mRootFrame field to RootActivity

--- app/src/main/java/com/crackncrunch/flowexample/RootActivity.java
+++ app/src/main/java/com/crackncrunch/flowexample/RootActivity.java
@@ -3,10 +3,13 @@ package com.crackncrunch.flowexample;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 import flow.Flow;
 
 public class RootActivity extends AppCompatActivity {
+
+    private FrameLayout mRootFrame;
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -21,5 +24,7 @@ public class RootActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root);
+
+        mRootFrame = (FrameLayout) findViewById(R.id.root_frame);
     }
 }
