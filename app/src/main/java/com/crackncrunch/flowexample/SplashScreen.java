059aa3589690c8c1a95491c15e66b065867481d5
Lilian Burdianov <lilian.burdianov@gmail.com>
2017-02-10 00:00:43 +0400

    Create SplashScreen class and layout

---
+++ app/src/main/java/com/crackncrunch/flowexample/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.crackncrunch.flowexample;
+
+import com.crackncrunch.flowexample.flow.Screen;
+
+import flow.ClassKey;
+
+/**
+ * Created by Lilian on 09-Feb-17.
+ */
+
+@Screen(R.layout.screen_splash)
+public class SplashScreen extends ClassKey {
+}
