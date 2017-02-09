e8e4862dc89e58d9680f3aeb73b649d136900804
Lilian Burdianov <lilian.burdianov@gmail.com>
2017-02-09 22:06:42 +0400

    Override attachBaseContext method in RootActivity

--- app/src/main/java/com/crackncrunch/flowexample/RootActivity.java
+++ app/src/main/java/com/crackncrunch/flowexample/RootActivity.java
@@ -1,9 +1,21 @@
 package com.crackncrunch.flowexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import flow.Flow;
 
 public class RootActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        newBase = Flow.configure(newBase, this)
+                .defaultKey(new RedScreen())
+                .dispatcher(new TreeKeyDispatcher())
+                .install();
+        super.attachBaseContext(newBase);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
